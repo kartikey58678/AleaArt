06f056ff09aa2e93953e7542d06b4fbd4fda8ea7
Jatin Sinha <jatinsinha03@gmail.com>
2025-10-25 03:01:23 +0530

    Modified the css of the landing page

--- frontend-aleart/src/app/layout.tsx
+++ frontend-aleart/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import Providers from '@/components/Providers';
-import Navigation from '@/components/Navigation';
 import './globals.css';
 
 export default function RootLayout({
@@ -9,9 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+      </head>
       <body>
         <Providers>
-          <Navigation />
           {children}
         </Providers>
       </body>
